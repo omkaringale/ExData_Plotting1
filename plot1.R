f233d68c24e8a5027b9188f274786235e2684d7c
omkaringale <51236863+omkaringale@users.noreply.github.com>
2019-07-08 02:33:28 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,3 @@
-graph<-read.table("household_power_consumption.txt",sep=";",header=TRUE)
+graph<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
 a<-subset(graph,graph$Date=="1/2/2007"|graph$Date=="2/2/2007")
 hist(as.numeric(a$Global_active_power),col = "red",xlab="Global Active Power",main="Global Active Power")
